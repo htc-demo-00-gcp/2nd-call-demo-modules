0b83004f23795431b3235c83862b85b04253a146
Mateusz Jenek <mateusz.jenek@gmail.com>
2025-08-21 14:12:00 +0200

    fix: service account

--- k8s-service-account/main.tf
+++ k8s-service-account/main.tf
@@ -20,4 +20,6 @@ resource "kubernetes_service_account" "service_account" {
       "iam.gke.io/gcp-service-account" = var.gcp_service_account_email
     }
   }
+  
+  automount_service_account_token = false
 }
