1f07e5941e68b632ecbfa35ca745717b6223bd26
Mateusz Jenek <mateusz.jenek@gmail.com>
2025-08-22 14:18:02 +0200

    feat: add workload identity binding

--- k8s-service-account/inputs.tf
+++ k8s-service-account/inputs.tf
@@ -7,3 +7,8 @@ variable "gcp_service_account_email" {
   description = "The email of the GCP service account to associate with the Kubernetes service account."
   type        = string
 }
+
+variable "project_id" {
+  description = "The GCP project ID where the Workload Identity binding will be created."
+  type        = string
+}
--- k8s-service-account/main.tf
+++ k8s-service-account/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.3"
     }
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0"
+    }
   }
 }
 
@@ -20,4 +24,11 @@ resource "kubernetes_service_account" "service_account" {
       "iam.gke.io/gcp-service-account" = var.gcp_service_account_email
     }
   }
+}
+
+# Enable Workload Identity binding between K8s SA and GCP SA
+resource "google_service_account_iam_member" "workload_identity_binding" {
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.gcp_service_account_email}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.project_id}.svc.id.goog[${var.namespace}/${kubernetes_service_account.service_account.metadata[0].name}]"
 }
